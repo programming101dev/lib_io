6470691c52de6b393ec1358af4bbfbd6ef22543d
programming101dev <113087924+programming101dev@users.noreply.github.com>
2026-08-06 13:27:27 -0700

    Enforce semantic wrapper boundaries

--- src/sys/uio.c
+++ src/sys/uio.c
@@ -14,12 +14,6 @@
  * limitations under the License.
  */
 
-#include "p101_io/p101_aio.h"
-#include "p101_io/p101_fcntl.h"
-#include "p101_io/p101_poll.h"
-#include "p101_io/p101_stdio.h"
-#include "p101_io/p101_unistd.h"
-#include "p101_io/sys/p101_select.h"
 #include "p101_io/sys/p101_uio.h"
 #include <p101_env/wrapper.h>
 
